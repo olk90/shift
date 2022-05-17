981ccbcee15594650572b6e06117c28ccebd6aa5
Oliver Köhler <olk.git@posteo.eu>
2022-05-17 22:11:52 +0200

    create logs directory of not exists

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,9 +1,12 @@
 import logging
+import os.path
 
 from logging.handlers import RotatingFileHandler
 
 
 def configure_file_handler(name: str) -> RotatingFileHandler:
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
     rfh = RotatingFileHandler("logs/{}.log".format(name), maxBytes=1024 * 1024, backupCount=5, encoding="utf-8")
     rfh.setLevel(logging.INFO)
     formatter = logging.Formatter(
